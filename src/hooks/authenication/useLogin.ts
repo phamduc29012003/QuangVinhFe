c2196e33e42aa178f4cf770fb4168e850e14c8c7
phamduc29012003 <pduc8060@gmail.com>
2025-11-24 16:54:09 +0700

    feat: Introduce structured error handling with error codes and centralized API endpoint management.

--- src/hooks/authenication/useLogin.ts
+++ src/hooks/authenication/useLogin.ts
@@ -2,17 +2,17 @@ import SonnerToaster from '@/components/ui/toaster'
 import { POST } from '@/core/api'
 import type { LoginFormData, LoginResponse } from '@/types/Auth'
 import { setTokenAuth } from '@/utils/auth'
-import { handleCommonError } from '@/utils/handleErrors'
 import { useMutation } from '@tanstack/react-query'
 import type { AxiosResponse } from 'axios'
 import { useNavigate } from 'react-router'
 import { useAuthStore } from '@/stores'
+import { API_ENDPOINT } from '@/common'
 export const useLogin = () => {
   const navigate = useNavigate()
   const setAuth = useAuthStore((state) => state.setAuth)
   const loginMutation = useMutation({
     mutationFn: async (data: LoginFormData) => {
-      const response = await POST('/api/auth/login', data)
+      const response = await POST(API_ENDPOINT.LOGIN, data)
       return response as AxiosResponse<LoginResponse>
     },
     onSuccess: (data) => {
@@ -25,10 +25,6 @@ export const useLogin = () => {
       })
       setAuth(user, token)
       navigate('/dashboard')
-    },
-    onError: (error) => {
-      console.log('error', error)
-      handleCommonError(error)
     },
   })
 
--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,12 +1,18 @@
 // src/lib/queryClient.ts
 import { QueryClient } from '@tanstack/react-query'
+import { handleCommonError } from '@/utils/handleErrors'
 
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false, // không tự refetch khi đổi tab
       retry: 1, // retry 1 lần nếu lỗi
-      staleTime: 1000 * 60 * 5, // cache trong 5 phút
+      staleTime: 1000 * 60 * 5,
+    },
+    mutations: {
+      onError: (error: unknown) => {
+        handleCommonError(error)
+      },
     },
   },
 })
